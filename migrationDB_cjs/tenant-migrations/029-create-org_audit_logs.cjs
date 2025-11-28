9301e61c05ab0a65b9b747a721eb7f24e2771e8f
Mathumitha <madhumitha.k@vivo.ai>
2025-11-28 10:29:16 +0530

    Create 029-create-org_audit_logs.cjs

---
+++ migrationDB_cjs/tenant-migrations/029-create-org_audit_logs.cjs
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("org_audit_logs", (table) => {
+    table.increments("audit_id").primary();       // INT (PK)
+    table.integer("org_id");                      // INT
+    table.integer("user_id");                     // INT
+    table.string("module");                       // VARCHAR
+    table.string("action");                       // VARCHAR
+    table.string("affected_entity_id");           // VARCHAR
+    table.text("details");                        // TEXT
+    table.timestamp("timestamp").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    table.json("details_json").nullable();        // JSON NULL
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("org_audit_logs");
+};
