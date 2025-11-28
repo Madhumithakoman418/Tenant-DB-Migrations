3c349659bbcd45974e84323f98286c13ab602051
Mathumitha <madhumitha.k@vivo.ai>
2025-11-28 10:28:13 +0530

    Create 015-create-clauses.cjs

---
+++ migrationDB_cjs/tenant-migrations/015-create-clauses.cjs
@@ -0,0 +1,25 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("clauses", (table) => {
+    table.increments("id").primary();                   // INT (PK)
+    table.string("name");                               // VARCHAR
+    table.text("description");                          // TEXT
+    table.text("justification");                        // TEXT
+    table.integer("avivo_rank").defaultTo(1);           // INT DEFAULT 1
+    table.string("risk_level");                         // VARCHAR
+    table.integer("clause_type_id");                    // INT
+    table.enu("source", ["internal", "external"]);      // ENUM
+    table.integer("org_id").nullable();                 // INT NULL
+    table.integer("user_id").nullable();                // INT NULL
+    table.text("categories").nullable();                // TEXT
+    table.timestamp("created_on").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    table.timestamp("updated_on").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    table.integer("created_by_user_id").nullable();     // INT NULL
+    table.integer("updated_by_user_id").nullable();     // INT NULL
+    table.boolean("is_archived").defaultTo(false);      // TINYINT DEFAULT 0
+    
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("clauses");
+};
