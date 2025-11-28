86d0f7d5bf0fdb18c790f91a56742243a6da1c54
Mathumitha <madhumitha.k@vivo.ai>
2025-11-28 10:28:42 +0530

    Create 021-create-qa_sessions.cjs

---
+++ migrationDB_cjs/tenant-migrations/021-create-qa_sessions.cjs
@@ -0,0 +1,22 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("qa_sessions", (table) => {
+        table.increments("session_id").primary();   // Integer (PK)
+
+        table.integer("user_id");                   // Integer
+        table.json("ci_file_guid_arr");             // JSON
+        table.json("tag_ids_arr");                  // JSON
+        table.integer("parent_session_id");         // Integer
+        table.timestamp("created_at");                        // Timestamp
+        table.timestamp("updated_at");   
+        table.bigInteger("archived_by").nullable();           // BIGINT NULL 
+        table.binary("title");                      // Blob
+        table.integer("chat_type");                 // Integer
+        table.string("connection_id");              // Varchar
+        table.string("ci_file_guid", 100);                    // Varchar(100)
+        table.integer("org_id");                    // Integer
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("qa_sessions");
+};
