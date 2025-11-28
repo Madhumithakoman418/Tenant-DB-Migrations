6d3bc0f8ecadf21f72a81634abe2e92d7b169b9c
Mathumitha <madhumitha.k@vivo.ai>
2025-11-28 10:28:46 +0530

    Create 022-create-qa_chats.cjs

---
+++ migrationDB_cjs/tenant-migrations/022-create-qa_chats.cjs
@@ -0,0 +1,23 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("qa_chats", (table) => {
+    table.increments("chat_id").primary();             // INT (PK)
+    table.integer("session_id");                       // INT
+    table.binary("question");                          // BLOB
+    table.binary("answer");                            // MEDIUMBLOB
+    table.timestamp("created_date").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    table.timestamp("updated_date").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    table.integer("is_archived").defaultTo(0);         // INT DEFAULT 0
+    table.text("error");                               // TEXT
+    table.string("ai_chat_type");                      // VARCHAR
+    table.json("ci_file_guid_arr");                    // JSON
+    table.string("request_id");                        // VARCHAR
+    table.bigInteger("file_id").nullable();            // BIGINT NULL
+    table.string("file_name").nullable();              // VARCHAR NULL
+    table.string("connection_id").nullable();          // VARCHAR NULL
+    
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("qa_chats");
+};
