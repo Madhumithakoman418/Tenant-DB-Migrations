4a23c53068a2a0e35887e647088105f47f422322
Mathumitha <madhumitha.k@vivo.ai>
2025-11-28 10:28:28 +0530

    Create 018-create-user_groups.cjs

---
+++ migrationDB_cjs/tenant-migrations/018-create-user_groups.cjs
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("user_groups", (table) => {
+        table.bigIncrements("group_id").primary();   // Bigint (PK)
+        table.integer("org_id");                            // INT
+        table.string("group_name");                  // Varchar
+        table.bigInteger("created_by");              // Bigint
+        table.timestamp("created_at");               // Timestamp
+        table.timestamp("updated_at");               // Timestamp
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("user_groups");
+};
