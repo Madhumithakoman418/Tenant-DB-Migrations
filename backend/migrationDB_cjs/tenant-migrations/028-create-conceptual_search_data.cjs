29829c716410c9e531528bd5ef027ed572698f6a
Mathumitha <madhumitha.k@vivo.ai>
2025-12-01 11:18:39 +0530

    Create 028-create-conceptual_search_data.cjs

---
+++ backend/migrationDB_cjs/tenant-migrations/028-create-conceptual_search_data.cjs
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("conceptual_search_data", (table) => {
+        table.increments("id").primary();        // Integer (PK)
+        table.integer("user_id");                // Integer
+        table.text("conceptual_search_query");   // Text
+        table.json("received_fileids");          // JSON
+        table.timestamp("inserted_at").defaultTo(knex.fn.now());    // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("conceptual_search_data");
+};
