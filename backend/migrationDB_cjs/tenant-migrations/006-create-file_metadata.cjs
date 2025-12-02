f26c6cd40a18a4e7d8efc272fcf4f407e0cc23ae
Mathumitha <madhumithakoman04@gmail.com>
2025-12-02 12:01:13 +0530

    Update 006-create-file_metadata.cjs

--- backend/migrationDB_cjs/tenant-migrations/006-create-file_metadata.cjs
+++ backend/migrationDB_cjs/tenant-migrations/006-create-file_metadata.cjs
@@ -2,11 +2,13 @@ exports.up = function (knex) {
   return knex.schema.createTable("file_metadata", (table) => {
     table.bigIncrements("data_id").primary();       // BIGINT (PK)
 
-    table.string("cl_file_guid");                   // VARCHAR
+    table.string("ci_file_guid");                   // VARCHAR
     table.string("metadata_title");                 // VARCHAR
     table.text("metadata_text_value");              // TEXT
     table.float("metadata_int_value");              // FLOAT
     table.decimal("metadata_decimal_value");        // DECIMAL
+    table.timestamp("updated_on").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
     table.bigInteger("tag_id");                     // BIGINT
     table.string("field");                          // VARCHAR - field name
     table.string("type");                           // VARCHAR - type of metadata
