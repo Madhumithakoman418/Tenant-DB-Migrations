f598e5311305d941f876dc5d7e8c5e53ec9a9c79
Mathumitha <madhumithakoman04@gmail.com>
2025-12-02 12:01:16 +0530

    Update 007-create-file_metadata_dates.cjs

--- backend/migrationDB_cjs/tenant-migrations/007-create-file_metadata_dates.cjs
+++ backend/migrationDB_cjs/tenant-migrations/007-create-file_metadata_dates.cjs
@@ -2,10 +2,9 @@ exports.up = function (knex) {
     return knex.schema.createTable("file_metadata_dates", (table) => {
         table.bigIncrements("data_id").primary();      // Bigint (PK)
 
-        table.string("cl_file_guid");                  // Varchar
+        table.string("ci_file_guid");                  // Varchar
         table.string("date_title");                    // Varchar
         table.dateTime("date_value");                  // Datetime
-        table.timestamp("created_on").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         table.timestamp("updated_on").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     });
 };
