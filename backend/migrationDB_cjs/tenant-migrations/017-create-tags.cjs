cb39c0e805daed25b596e9220d7bf179c2a7dd9f
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 13:40:10 +0530

    Update 017-create-tags.cjs

--- backend/migrationDB_cjs/tenant-migrations/017-create-tags.cjs
+++ backend/migrationDB_cjs/tenant-migrations/017-create-tags.cjs
@@ -2,11 +2,11 @@ exports.up = function (knex) {
     return knex.schema.createTable("tags", (table) => {
         table.increments("tag_id").primary();     // Integer (PK)
 
-        table.string("name");                     // Varchar
+        table.string("name").notNullable();                     // Varchar
         table.text("description");                // Text
-        table.enu("tag_type", ['template','clause','user_collection']); // Enum
-        table.integer("user_id");                 // Integer
-        table.boolean("is_default");              // Tinyint
+        table.enu("tag_type", ['template','clause','user_collection']).notNullable(); // Enum
+        table.integer("user_id").defaultTo(null);                 // Integer
+        table.boolean("is_default").notNullable().defaultTo(0);              // Tinyint
     });
 };
 
