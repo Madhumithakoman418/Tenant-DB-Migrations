f80efa3714ac0ec538fd04c1a79ad1d4bb6d6679
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 13:40:21 +0530

    Update 020-create-access_control_master.cjs

--- backend/migrationDB_cjs/tenant-migrations/020-create-access_control_master.cjs
+++ backend/migrationDB_cjs/tenant-migrations/020-create-access_control_master.cjs
@@ -2,15 +2,15 @@ exports.up = function (knex) {
     return knex.schema.createTable("access_control_master", (table) => {
         table.bigIncrements("acm_id").primary();              // Bigint (PK)
 
-        table.string("ci_file_guid", 100);                    // Varchar(100)
-        table.bigInteger("tag_id");                           // Bigint
-        table.bigInteger("group_id");                         // Bigint
-        table.bigInteger("user_id");                          // Bigint
+        table.string("ci_file_guid", 100).defaultTo(null);                    // Varchar(100)
+        table.bigInteger("tag_id").defaultTo(null);                           // Bigint
+        table.bigInteger("group_id").defaultTo(null);                         // Bigint
+        table.bigInteger('user_id').nullable().comment('Represents the tag-user linkage; stores the user ID associated with a tag for access control tracking.');   // Bigint
 
-        table.bigInteger("shared_with");                      // Bigint
-        table.bigInteger("shared_by_user_id");                // Bigint
-        table.bigInteger("source_tag_id");                    // Bigint
-        table.bigInteger("perm_id");                          // Bigint
+        table.bigInteger("shared_with").defaultTo(null);                      // Bigint
+        table.bigInteger("shared_by_user_id").defaultTo(null);                // Bigint
+        table.bigInteger("source_tag_id").defaultTo(null);                    // Bigint
+        table.bigInteger("perm_id").defaultTo(null);                          // Bigint
 
         table.timestamp("created_at");                        // Timestamp
         table.timestamp("updated_at");                        // Timestamp
