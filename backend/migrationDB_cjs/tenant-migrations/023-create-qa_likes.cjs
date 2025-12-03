d5d325aca4a1bdf7b476e22c126d15bb584124a8
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 16:02:20 +0530

    Update 023-create-qa_likes.cjs

--- backend/migrationDB_cjs/tenant-migrations/023-create-qa_likes.cjs
+++ backend/migrationDB_cjs/tenant-migrations/023-create-qa_likes.cjs
@@ -2,9 +2,9 @@ exports.up = function (knex) {
     return knex.schema.createTable("qa_likes", (table) => {
         table.increments("data_id").primary();  // Integer (PK)
 
-        table.integer("chat_id");               // Integer
-        table.integer("user_id");               // Integer
-        table.integer("is_liked");              // Integer
+        table.integer("chat_id").nullable();               // Integer
+        table.integer("user_id").nullable();               // Integer
+        table.integer("is_liked").nullable();              // Integer
     });
 };
 
