365a77fdf7a216e95c0786e190454d65823be2d8
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 16:02:12 +0530

    Update 021-create-qa_sessions.cjs

--- backend/migrationDB_cjs/tenant-migrations/021-create-qa_sessions.cjs
+++ backend/migrationDB_cjs/tenant-migrations/021-create-qa_sessions.cjs
@@ -2,17 +2,17 @@ exports.up = function (knex) {
     return knex.schema.createTable("qa_sessions", (table) => {
         table.increments("session_id").primary();   // Integer (PK)
 
-        table.integer("user_id");                   // Integer
-        table.json("ci_file_guid_arr");             // JSON
-        table.json("tag_ids_arr");                  // JSON
-        table.integer("parent_session_id");         // Integer
+        table.integer("user_id").defaultTo(null);                   // Integer
+        table.json("ci_file_guid_arr").nullable();             // JSON
+        table.json("tag_ids_arr").nullable();                  // JSON
+        table.integer("parent_session_id").nullable();         // Integer
         table.timestamp("created_date");                        // Timestamp
         table.timestamp("updated_date");   
         table.bigInteger("archived_by").nullable();           // BIGINT NULL 
         table.binary("title");                      // Blob
-        table.integer("chat_type");                 // Integer
-        table.string("connection_id");              // Varchar
-        table.string("ci_file_guid", 100);                    // Varchar(100)
+        table.integer("chat_type").defaultTo(0);                 // Integer
+        table.string("connection_id").nullable();              // Varchar
+        table.string("ci_file_guid", 100).nullable();                    // Varchar(100)
 
     });
 };
