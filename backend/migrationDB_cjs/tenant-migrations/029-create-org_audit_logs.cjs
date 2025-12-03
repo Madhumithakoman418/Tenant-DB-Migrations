e331ddd8a32e4991daa14c09d52cc1e6d25b51c8
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 17:13:40 +0530

    Update 029-create-org_audit_logs.cjs

--- backend/migrationDB_cjs/tenant-migrations/029-create-org_audit_logs.cjs
+++ backend/migrationDB_cjs/tenant-migrations/029-create-org_audit_logs.cjs
@@ -1,10 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.createTable("org_audit_logs", (table) => {
     table.increments("audit_id").primary();       // INT (PK)
-    table.integer("user_id");                     // INT
-    table.string("module");                       // VARCHAR
-    table.string("action");                       // VARCHAR
-    table.string("affected_entity_id");           // VARCHAR
+    table.integer("user_id").nullable();                     // INT
+    table.string("module").nullable();                       // VARCHAR
+    table.string("action").nullable();                       // VARCHAR
+    table.string("affected_entity_id").nullable();           // VARCHAR
     table.text("details");                        // TEXT
     table.timestamp("timestamp").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.json("details_json").nullable();        // JSON NULL
