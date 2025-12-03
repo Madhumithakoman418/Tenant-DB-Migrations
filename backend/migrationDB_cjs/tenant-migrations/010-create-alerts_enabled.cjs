aa5b170103b90b2803a32fdf91224a68419674dc
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 13:00:04 +0530

    Update 010-create-alerts_enabled.cjs

--- backend/migrationDB_cjs/tenant-migrations/010-create-alerts_enabled.cjs
+++ backend/migrationDB_cjs/tenant-migrations/010-create-alerts_enabled.cjs
@@ -2,10 +2,10 @@ exports.up = function (knex) {
     return knex.schema.createTable("alerts_enabled", (table) => {
         table.bigIncrements("alert_enabled_id").primary();  // Bigint (PK)
 
-        table.bigInteger("config_id");                      // Bigint
-        table.bigInteger("alert_type_id");                  // Bigint
-        table.integer("is_enabled");                        // Integer
-        table.integer("days_before");                       // Integer
+        table.bigInteger("config_id").defaultTo(null);                      // Bigint
+        table.bigInteger("alert_type_id").defaultTo(null);                  // Bigint
+        table.integer("is_enabled").defaultTo(null);                        // Integer
+        table.integer("days_before").default(null);                       // Integer
         table.timestamp("updated_on").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT now()
     });
 };
