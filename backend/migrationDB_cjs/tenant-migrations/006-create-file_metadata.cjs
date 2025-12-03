0984e8592bfb32bc590ab45a0caf9caffa7c6283
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 12:59:48 +0530

    Update 006-create-file_metadata.cjs

--- backend/migrationDB_cjs/tenant-migrations/006-create-file_metadata.cjs
+++ backend/migrationDB_cjs/tenant-migrations/006-create-file_metadata.cjs
@@ -5,14 +5,14 @@ exports.up = function (knex) {
     table.string("ci_file_guid");                   // VARCHAR
     table.string("metadata_title");                 // VARCHAR
     table.text("metadata_text_value");              // TEXT
-    table.float("metadata_int_value");              // FLOAT
-    table.decimal("metadata_decimal_value");        // DECIMAL
+    table.float("metadata_int_value").notNullable().defaultTo(0); // FLOAT default(0)
+    table.decimal("metadata_decimal_value", 15, 8).nullable(); // DECIMALdefault NULL
     table.timestamp("updated_on").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
-    table.bigInteger("tag_id");                     // BIGINT
+    table.bigInteger("tag_id").defaultTo(0);                  // BIGINT DEFAULT (0)
     table.string("field");                          // VARCHAR - field name
     table.string("type");                           // VARCHAR - type of metadata
-    table.boolean("has_recurring_payment").defaultTo(false); // TINYINT(1) DEFAULT 0
+    table.boolean("has_recurring_payment").defaultTo(null); // TINYINT(1) DEFAULT NULL
   });
 };
 
