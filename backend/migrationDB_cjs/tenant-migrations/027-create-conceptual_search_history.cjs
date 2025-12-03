bdbce2ef3cb6617ae8d4afdbc375f4ecc4931de4
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 17:13:33 +0530

    Update 027-create-conceptual_search_history.cjs

--- backend/migrationDB_cjs/tenant-migrations/027-create-conceptual_search_history.cjs
+++ backend/migrationDB_cjs/tenant-migrations/027-create-conceptual_search_history.cjs
@@ -2,8 +2,8 @@ exports.up = function (knex) {
     return knex.schema.createTable("conceptual_search_history", (table) => {
         table.increments("id").primary();        // Integer (PK)
 
-        table.integer("user_id");                // Integer
-        table.text("conceptual_search_query");   // Text
+        table.integer("user_id").notNullable();                // Integer
+        table.text("conceptual_search_query").notNullable();   // Text
         table.timestamp("inserted_at").defaultTo(knex.fn.now());    // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     });
 };
