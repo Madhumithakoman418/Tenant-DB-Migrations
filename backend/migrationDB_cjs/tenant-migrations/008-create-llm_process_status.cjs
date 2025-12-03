62d783d249e429dd6cd1c3f8bb22141b21e35e31
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 12:59:55 +0530

    Update 008-create-llm_process_status.cjs

--- backend/migrationDB_cjs/tenant-migrations/008-create-llm_process_status.cjs
+++ backend/migrationDB_cjs/tenant-migrations/008-create-llm_process_status.cjs
@@ -2,18 +2,19 @@ exports.up = function (knex) {
   return knex.schema.createTable("llm_process_status", (table) => {
     table.bigIncrements("process_id").primary();           // BIGINT (PK)
     table.string("ci_file_guid");                          // VARCHAR
-    table.boolean("retryable_file").defaultTo(false);      // TINYINT(1) DEFAULT 0
-    table.integer("completed_steps").nullable();           // INT
-    table.text("error_msg").nullable();                    // TEXT
-    table.integer("retry_count").defaultTo(0);             // INT DEFAULT 0
-    table.integer("process_type").nullable();              // INT
+    table.boolean("retryable_file").defaultTo(false);      // TINYINT(1) DEFAULT 0 
+    table.integer("completed_steps").unsigned().notNullable().defaultTo(0); // INT UNSIGNED NOT NULL DEFAULT 0
+    table.text("error_msg");                    // TEXT
+    table.integer("retry_count").unsigned().notNullable().defaultTo(0);            // INT UNSIGNED NOT NULL DEFAULT 0
+    table.integer("process_type").unsigned().notNullable();              // INT  UNSIGNED NOT NULL
    
     table.timestamp("created_time").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.timestamp("updated_time").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.timestamp("process_start_time").nullable();          // TIMESTAMP NULL
     table.timestamp("process_end_time").nullable();            // TIMESTAMP NULL
-    table.boolean("in_queue").defaultTo(false);                // TINYINT(1) DEFAULT 0
-    table.integer("execution_progress_percent").nullable();// INT
+    table.boolean("in_queue").defaultTo(true);                // TINYINT(1) DEFAULT 1
+    table.integer("execution_progress_percent").notNullable().defaultTo(0);  // int NOT NULL DEFAULT 0
+
   });
 };
 
