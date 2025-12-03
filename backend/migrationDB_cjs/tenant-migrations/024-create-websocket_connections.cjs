5de03da896a67fe212f17c7c306abe7db709c556
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 16:02:24 +0530

    Update 024-create-websocket_connections.cjs

--- backend/migrationDB_cjs/tenant-migrations/024-create-websocket_connections.cjs
+++ backend/migrationDB_cjs/tenant-migrations/024-create-websocket_connections.cjs
@@ -2,11 +2,11 @@ exports.up = function (knex) {
     return knex.schema.createTable("websocket_connections", (table) => {
         table.increments("id").primary();           // Integer (PK)
 
-        table.string("connection_id");              // Varchar
+        table.string("connection_id").notNullable();              // Varchar
         table.timestamp("created_at").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         table.timestamp("updated_at").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        table.string("user_id");                    // Varchar
-        table.string("client_ip");                  // Varchar
+        table.string("user_id").Nullable();                    // Varchar
+        table.string("client_ip").Nullable();                  // Varchar
     });
 };
 
