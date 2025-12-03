a2a3f4202db062b23953d0da524a394db97727b1
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 12:10:27 +0530

    Update 004-create-role_permission.cjs

--- backend/migrationDB_cjs/tenant-migrations/004-create-role_permission.cjs
+++ backend/migrationDB_cjs/tenant-migrations/004-create-role_permission.cjs
@@ -2,10 +2,11 @@ exports.up = function (knex) {
     return knex.schema.createTable("role_permissions", (table) => {
         table.bigIncrements("role_perm_id").primary();   // Bigint (PK)
 
-        table.integer("role_id");                        // Integer
-        table.integer("com_id");                         // Integer
-        table.integer("perm_id");                        // Integer
-        table.boolean("is_enabled");                     // Tinyint
+       table.integer("role_id").notNullable();       // INT NOT NULL
+       table.integer("com_id").notNullable();        // INT NOT NULL
+       table.integer("perm_id").notNullable();       // INT NOT NULL
+       table.boolean("is_enabled").defaultTo(true);  // TINYINT(1) DEFAULT '1'
+
         table.timestamp("created_date").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         table.timestamp("updated_date").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     });
