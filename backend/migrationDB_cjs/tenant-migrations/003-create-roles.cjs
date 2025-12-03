931f48c0d12ec66a87d99ad8506bb4862aa6e8ca
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 12:10:23 +0530

    Update 003-create-roles.cjs

--- backend/migrationDB_cjs/tenant-migrations/003-create-roles.cjs
+++ backend/migrationDB_cjs/tenant-migrations/003-create-roles.cjs
@@ -4,7 +4,7 @@ exports.up = function (knex) {
 
         table.string("rolename");                        // Varchar
         table.enu("category", ["ADMIN", "USER", "OWNER", "VIEWER"]); // Enum (example values)
-        table.integer("user_id");                    // Integer
+        table.integer("user_id").nullable().defaultTo(null); // default null
         table.timestamp("created_on").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         table.timestamp("updated_on").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     });
