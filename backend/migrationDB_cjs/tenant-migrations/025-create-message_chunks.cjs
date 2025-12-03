d7652c620020de1954141a945af882aeb8b80ee3
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 16:02:28 +0530

    Update 025-create-message_chunks.cjs

--- backend/migrationDB_cjs/tenant-migrations/025-create-message_chunks.cjs
+++ backend/migrationDB_cjs/tenant-migrations/025-create-message_chunks.cjs
@@ -1,15 +1,15 @@
 exports.up = function (knex) {
   return knex.schema.createTable("message_chunks", (table) => {
     table.increments("id").primary();                  // INT (PK)
-    table.integer("user_id");                          // INT
-    table.integer("session_id");                       // INT
-    table.string("connection_id");                     // VARCHAR
-    table.string("request_id");                        // VARCHAR
-    table.integer("chunk_order");                      // INT
+    table.integer("user_id").nullable();                          // INT
+    table.integer("session_id").nullable();                       // INT
+    table.string("connection_id").nullable();                     // VARCHAR
+    table.string("request_id").nullable();                        // VARCHAR
+    table.integer("chunk_order").nullable();                      // INT
     table.binary("chunk_content");                     // MEDIUMBLOB
-    table.text("error").nullable();                    // TEXT (stores error messages if any)
-    table.boolean("stream_completed_status");          // TINYINT
-    table.string("chat_id");                           // VARCHAR
+    table.text("error");                    // TEXT 
+    table.boolean("stream_completed_status").nullable();          // TINYINT
+    table.string("chat_id").nullable();                           // VARCHAR
     table.timestamp("created_date").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.timestamp("updated_date").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.timestamp("inserted_at").defaultTo(knex.fn.now());    // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
