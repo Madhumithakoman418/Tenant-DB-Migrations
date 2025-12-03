27758ef6dbdd7bb85a645be589aa5714e9903c7f
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 13:39:55 +0530

    Update 013-create-clause_types.cjs

--- backend/migrationDB_cjs/tenant-migrations/013-create-clause_types.cjs
+++ backend/migrationDB_cjs/tenant-migrations/013-create-clause_types.cjs
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
     return knex.schema.createTable("clause_types", (table) => {
         table.increments("id").primary();         // Integer (PK)
-        table.string("type");                     // Varchar
-        table.enu("source", ['AVIVO','ORGANIZATION','USER']); // Enum
+        table.string("type").notNullable();                     // Varchar
+        table.enu("source", ['AVIVO','ORGANIZATION','USER']).notNullable(); // Enum
         table.timestamp("created_at").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         table.timestamp("updated_at").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        table.integer("created_by_user_id");      // Integer
-        table.integer("updated_by_user_id");      // Integer
+        table.integer("created_by_user_id").defaultTo(null);      // Integer
+        table.integer("updated_by_user_id").defaultTo(null);      // Integer
     });
 };
 
