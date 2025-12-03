bb9aa9c9718e52877efe063552afba1deb9d70e1
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 13:40:13 +0530

    Update 018-create-user_groups.cjs

--- backend/migrationDB_cjs/tenant-migrations/018-create-user_groups.cjs
+++ backend/migrationDB_cjs/tenant-migrations/018-create-user_groups.cjs
@@ -1,10 +1,10 @@
 exports.up = function (knex) {
     return knex.schema.createTable("user_groups", (table) => {
         table.bigIncrements("group_id").primary();   // Bigint (PK)
-        table.string("group_name");                  // Varchar
-        table.bigInteger("created_by");              // Bigint
-        table.timestamp("created_at");               // Timestamp
-        table.timestamp("updated_at");               // Timestamp
+        table.string("group_name").notNullable();                  // Varchar
+        table.bigInteger('created_by').notNullable().comment('User ID of the group creator. Assumes a link to a users table.');  //bigint
+        table.timestamp('created_at').nullable().defaultTo(knex.fn.now());    //timestamp
+        table.timestamp('updated_at').nullable().defaultTo(knex.fn.now()).onUpdate(knex.fn.now()); //timestamp
     });
 };
 
