60a36f821eb752f77aa21bc2360bb632bb09be66
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 17:13:28 +0530

    Update 026-create-message_chunks_log.cjs

--- backend/migrationDB_cjs/tenant-migrations/026-create-message_chunks_log.cjs
+++ backend/migrationDB_cjs/tenant-migrations/026-create-message_chunks_log.cjs
@@ -2,17 +2,17 @@ exports.up = function (knex) {
     return knex.schema.createTable("message_chunks_log", (table) => {
         table.increments("id").primary();                // Int (PK)
 
-        table.integer("user_id");                        // Int
-        table.integer("session_id");                     // Int
-        table.string("connection_id", 255);              // Varchar(255)
-        table.string("request_id", 255);                 // Varchar(255)
-        table.integer("chunk_order");                    // Int
-        table.binary("chunk_content");                   // Mediumblob
+        table.integer("user_id").nullable();                        // Int
+        table.integer("session_id").nullable();                     // Int
+        table.string("connection_id", 255).nullable();              // Varchar(255)
+        table.string("request_id", 255).nullable();                 // Varchar(255)
+        table.integer("chunk_order").nullable();                    // Int
+        table.binary("chunk_content").nullable();                   // Mediumblob
         table.text("error");                             // Text
-        table.boolean("stream_completed_status");        // Tinyint(1)
-        table.timestamp("created_date");                 // Timestamp
-        table.timestamp("updated_date");                 // Timestamp
-        table.string("chat_id", 255);                    // Varchar(255)
+        table.boolean("stream_completed_status").nullable();        // Tinyint(1)
+        table.timestamp("created_date").defaultTo(knex.fn.now());                 // Timestamp
+        table.timestamp("updated_date").defaultTo(knex.fn.now());                 // Timestamp
+        table.string("chat_id", 255).nullable();                    // Varchar(255)
     });
 };
 
