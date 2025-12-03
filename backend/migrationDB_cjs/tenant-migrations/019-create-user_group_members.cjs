3411446821842e19ef345c243ae32e2a88924651
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 13:40:17 +0530

    Update 019-create-user_group_members.cjs

--- backend/migrationDB_cjs/tenant-migrations/019-create-user_group_members.cjs
+++ backend/migrationDB_cjs/tenant-migrations/019-create-user_group_members.cjs
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
     return knex.schema.createTable("user_group_members", (table) => {
-        table.bigInteger("user_member_id");                // Bigint (FK)
-        table.bigInteger("group_id");                // Bigint (FK)
-        table.bigInteger("user_id");                 // Bigint (FK)
-        table.timestamp("created_at");               // Timestamp
+        table.bigInteger("user_member_id").notNullable();                // Bigint (FK)
+        table.bigInteger("group_id").notNullable();                // Bigint (FK)
+        table.bigInteger("user_id").notNullable();                 // Bigint (FK)
+        table.timestamp('created_at').nullable().defaultTo(knex.fn.now()).comment('Timestamp for when the user was added.'); // Timestamp
     });
 };
 
