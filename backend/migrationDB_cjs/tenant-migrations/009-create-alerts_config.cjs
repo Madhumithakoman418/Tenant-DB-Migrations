6127746f69b1abd4a328e12a14f4cce0fba75d61
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 12:59:59 +0530

    Update 009-create-alerts_config.cjs

--- backend/migrationDB_cjs/tenant-migrations/009-create-alerts_config.cjs
+++ backend/migrationDB_cjs/tenant-migrations/009-create-alerts_config.cjs
@@ -2,8 +2,8 @@ exports.up = function (knex) {
     return knex.schema.createTable("alerts_config", (table) => {
         table.bigIncrements("config_id").primary();   // Bigint (PK)
 
-        table.string("ci_org_guid");                  // Varchar
-        table.integer("send_alerts");                 // Integer
+        table.string("ci_org_guid").defaultTo(null);                  // Varchar
+        table.integer("send_alerts").defaultTo(null);                 // Integer
         table.text("email_ids");                      // Text
         table.timestamp("updated_on").defaultTo(knex.fn.now()); // TIMESTAMP DEFAULT now()
     });
