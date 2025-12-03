edd07ec3fa496ea2b04a75781ea60379a3414f91
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 16:02:16 +0530

    Update 022-create-qa_chats.cjs

--- backend/migrationDB_cjs/tenant-migrations/022-create-qa_chats.cjs
+++ backend/migrationDB_cjs/tenant-migrations/022-create-qa_chats.cjs
@@ -1,20 +1,20 @@
 exports.up = function (knex) {
   return knex.schema.createTable("qa_chats", (table) => {
     table.increments("chat_id").primary();             // INT (PK)
-    table.integer("session_id");                       // INT
+    table.integer("session_id").nullable();                       // INT
     table.binary("question");                          // BLOB
     table.binary("answer");                            // MEDIUMBLOB
     table.timestamp("created_date").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.timestamp("updated_date").defaultTo(knex.fn.now());  // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    table.integer("is_archived").defaultTo(0);         // INT DEFAULT 0
+    table.integer("is_archived").nullable();         // INT DEFAULT null
     table.text("error");                               // TEXT
-    table.string("ai_chat_type");                      // VARCHAR
-    table.json("ci_file_guid_arr");                    // JSON
-    table.string("request_id");                        // VARCHAR
+    table.string("ai_chat_type").nullable();                      // VARCHAR
+    table.json("ci_file_guid_arr").nullable();                    // JSON
     table.bigInteger("file_id").nullable();            // BIGINT NULL
     table.string("file_name").nullable();              // VARCHAR NULL
+    table.string("request_id").nullable();                        // VARCHAR
     table.string("connection_id").nullable();          // VARCHAR NULL
-    table.string("request_id").nullable();                // VARCHAR NULL
+    
     
   });
 };
