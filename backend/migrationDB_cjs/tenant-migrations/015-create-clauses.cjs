adfd1c15f4d2f8e6a6eafdb5cff6de56a633c613
Mathumitha <madhumithakoman04@gmail.com>
2025-12-03 13:40:02 +0530

    Update 015-create-clauses.cjs

--- backend/migrationDB_cjs/tenant-migrations/015-create-clauses.cjs
+++ backend/migrationDB_cjs/tenant-migrations/015-create-clauses.cjs
@@ -1,21 +1,21 @@
 exports.up = function (knex) {
   return knex.schema.createTable("clauses", (table) => {
     table.increments("id").primary();                   // INT (PK)
-    table.string("name");                               // VARCHAR
+    table.string("name").defaultTo(null);                               // VARCHAR
     table.text("description");                          // TEXT
     table.text("justification");                        // TEXT
     table.integer("avivo_rank").defaultTo(1);           // INT DEFAULT 1
-    table.string("risk_level");                         // VARCHAR
-    table.string("jurisdiction");                         // VARCHAR
-    table.integer("clause_type_id");                    // INT
-    table.enu("source", ['AVIVO','ORGANIZATION','USER']);      // ENUM
+    table.string("risk_level").defaultTo(null);                         // VARCHAR
+    table.string("jurisdiction").defaultTo(null);                         // VARCHAR
+    table.integer("clause_type_id").defaultTo(null);                    // INT
+    table.enu("source", ['AVIVO','ORGANIZATION','USER']).notNullable().defaultTo('AVIVO');      // ENUM
     table.integer("user_id").nullable();                // INT NULL
     table.text("categories").nullable();                // TEXT
     table.timestamp("created_at").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.timestamp("updated_at").defaultTo(knex.fn.now());   // TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     table.integer("created_by_user_id").nullable();     // INT NULL
     table.integer("updated_by_user_id").nullable();     // INT NULL
-    table.boolean("is_archived").defaultTo(false);      // TINYINT DEFAULT 0
+    table.boolean("is_archived").defaultTo(null);      // TINYINT DEFAULT NULL
     
   });
 };
